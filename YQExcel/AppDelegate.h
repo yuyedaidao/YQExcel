a1efcd16359ab1bfa619aa963a9087d586306f24
Wang <wyqpadding@gmail.com>
2017-05-16 10:32:38 +0800

    Initial Commit

---
+++ YQExcel/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YQExcel
+//
+//  Created by Wang on 2017/5/16.
+//  Copyright © 2017年 Wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YQExcel/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YQExcel
+//
+//  Created by Wang on 2017/5/16.
+//  Copyright © 2017年 Wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
