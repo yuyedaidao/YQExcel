1672c1bbfb81c733af91493eeedb9a52956ee988
Wang <wyqpadding@gmail.com>
2017-05-19 17:26:11 +0800

    打个标签, 准备用新方案,覆盖一个视图标记选中

---
+++ Classes/YQCoverView.h
@@ -0,0 +1,15 @@
+//
+//  YQCoverView.h
+//  YQExcel
+//
+//  Created by Wang on 2017/5/19.
+//  Copyright © 2017年 Wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface YQCoverView : UIView
+
+@property (strong, nonatomic) UIColor *edgeColor;
+
+@end
---
+++ Classes/YQCoverView.m
@@ -0,0 +1,36 @@
+//
+//  YQCoverView.m
+//  YQExcel
+//
+//  Created by Wang on 2017/5/19.
+//  Copyright © 2017年 Wang. All rights reserved.
+//
+
+#import "YQCoverView.h"
+
+@implementation YQCoverView
+
+
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+    CGContextRef context = UIGraphicsGetCurrentContext();
+    CGContextAddRect(context, self.bounds);
+    CGContextSetLineWidth(context, 2);
+    if (_edgeColor) {
+        [_edgeColor setStroke];
+    } else {
+        [UIColor colorWithRed:80 / 255.0f green:157 / 255.0f blue:234 / 255.0f alpha:1];
+    }
+    CGContextStrokePath(context);
+
+}
+
+
+- (void)setEdgeColor:(UIColor *)edgeColor {
+    if (_edgeColor != edgeColor) {
+        _edgeColor = edgeColor;
+        [self setNeedsDisplay];
+    }
+}
+
+@end
